a61edc97c84cc3118f8d8163b171117b6f0e62aa
Mauroruffo <mauroruffo16@gmail.com>
2024-02-12 16:52:02 -0600

    Base connection files

---
+++ IMDB_Project/Data/Conexion.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMDB_Project.Data
+{
+    public class Conexion
+    {
+
+        private string Base;
+        private string Servidor;
+        private string Usuario;
+        private string Clave;
+        private static Conexion Con = null;
+
+        private Conexion()
+        {
+            this.Base = "IMDB";
+            this.Servidor = "LTMEX2023012501";
+            this.Usuario = "TecMi";
+            this.Clave = "StandardE80";
+        }
+
+        public SqlConnection CrearConexion()
+        {
+            SqlConnection Cadena = new SqlConnection();
+            try
+            {
+                Cadena.ConnectionString = "Server=" + this.Servidor +
+                                        "; Database=" + this.Base +
+                                        "; User Id=" + this.Usuario +
+                                        "; Password=" + this.Clave;
+            }
+            catch (Exception ex)
+            {
+                Cadena = null;
+                throw ex;
+            }
+            return Cadena;
+        }
+
+        public static Conexion getInstancia()
+        {
+            if (Con == null)
+            {
+                Con = new Conexion();
+            }
+            return Con;
+        }
+
+    }
+}
