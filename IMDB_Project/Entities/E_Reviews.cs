63cf9c259dc4b2dcf7f87fb344d91c19d0c238de
davilaalex80 <156226701+davilaalex80@users.noreply.github.com>
2024-02-13 11:17:09 -0600

    added E_Reviews.cs file declaring Movies table

---
+++ IMDB_Project/Entities/E_Reviews.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMDB_Project.Entities
+{
+    public class E_Reviews
+    {
+        public int id_movie { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public int Year { get; set; }
+        public string Director { get; set; }
+        public int Length_min { get; set; }
+        public int id_studio { get; set; }
+
+    }
+}
