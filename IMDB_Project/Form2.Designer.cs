85983ddf2ef3d8950fcd4f3e19daba97aded669d
Mauroruffo <mauroruffo16@gmail.com>
2024-02-13 11:42:10 -0600

    Push Test

--- IMDB_Project/Form2.Designer.cs
+++ IMDB_Project/Form2.Designer.cs
@@ -36,9 +36,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(57, 62);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(39, 16);
+            this.label1.Size = new System.Drawing.Size(73, 16);
             this.label1.TabIndex = 0;
-            this.label1.Text = "User:";
+            this.label1.Text = "Username:";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form2
