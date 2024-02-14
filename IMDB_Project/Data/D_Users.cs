cc1cd6bc6fd55f06824623bd76c6d11cb34584f4
Mauroruffo <mauroruffo16@gmail.com>
2024-02-13 19:37:29 -0600

    Added "Add Review"

---
+++ IMDB_Project/Data/D_Users.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMDB_Project.Data
+{
+    public class D_Users
+    {
+        //Select users table
+        public DataTable List_Users()
+        {
+            SqlDataReader Resultado;
+            DataTable Tabla = new DataTable();
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                SqlCon = Conexion.getInstancia().CrearConexion();
+                //stored procedure 
+                SqlCommand Comando = new SqlCommand("usp_List_Users", SqlCon);
+                Comando.CommandType = CommandType.StoredProcedure;
+                SqlCon.Open();
+                Resultado = Comando.ExecuteReader();
+                Tabla.Load(Resultado);
+                return Tabla;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+        }
+
+    }
+}
